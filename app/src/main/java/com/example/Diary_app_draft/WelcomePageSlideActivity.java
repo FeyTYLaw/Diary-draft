9a5d4e94598fdbf46b770e0d6ffd356787d716dd
Fey <97257705+lawfey@users.noreply.github.com>
2022-03-12 00:44:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/Diary_app_draft/WelcomePageSlideActivity.java
@@ -0,0 +1,14 @@
+package com.example.Diary_app_draft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomePageSlideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page_slide);
+    }
+}
