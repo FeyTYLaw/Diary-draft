4eff1ba0e8aa3acc3f26e74420720c82eb24cf75
Fey <97257705+lawfey@users.noreply.github.com>
2022-03-22 19:46:11 -0400

    Fey TY Law

--- app/src/main/java/com/example/Diary_app_draft/SignupActivity.java
+++ app/src/main/java/com/example/Diary_app_draft/SignupActivity.java
@@ -3,6 +3,11 @@ package com.example.Diary_app_draft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        EditText username = (EditText) findViewById(R.id.username);
+        MaterialButton registerButton = (MaterialButton) findViewById(R.id.signupbutton);
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String username1 = username.getText().toString();
+                Toast.makeText(SignupActivity.this,"Username is" + username1,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
