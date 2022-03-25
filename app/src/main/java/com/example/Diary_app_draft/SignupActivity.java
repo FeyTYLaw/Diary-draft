b38697a4c9de2c0a5d9d7e82e750ffb16e1a76d1
Fey <97257705+lawfey@users.noreply.github.com>
2022-03-25 12:46:45 -0400

    Fey TY Law

--- app/src/main/java/com/example/Diary_app_draft/SignupActivity.java
+++ app/src/main/java/com/example/Diary_app_draft/SignupActivity.java
@@ -1,6 +1,9 @@
 package com.example.Diary_app_draft;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
@@ -14,17 +17,54 @@ import com.google.android.material.button.MaterialButton;
 
 public class SignupActivity extends AppCompatActivity {
     public Button button;
+    EditText username;
+    EditText email;
+    EditText password;
+    EditText confirmPassword;
+    Button register;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirm_password);
+        email = findViewById(R.id.email);
+        register = findViewById(R.id.signupbutton);
 
-        button = (Button) findViewById(R.id.signupbutton);
+        //button = (Button) findViewById(R.id.signupbutton);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chekDataEntered();
+            }
+        });
+    }
+
+    public boolean isEmpty(EditText text){
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    public boolean isEmail(EditText text){
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+    void chekDataEntered() {
+        if(isEmpty(username)){
+            Toast t = Toast.makeText(this,"Username is required!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+        if (isEmail(email) == false){
+            email.setError("Enter Valid email!");
+        }
+    }
 
 
-        EditText username = (EditText) findViewById(R.id.username);
+  /*      EditText username = (EditText) findViewById(R.id.username);
         MaterialButton registerButton = (MaterialButton) findViewById(R.id.signupbutton);
 
         registerButton.setOnClickListener(new View.OnClickListener(){
@@ -34,7 +74,7 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(intent);
                 String username1 = username.getText().toString();
                 Toast.makeText(SignupActivity.this,"Username is" + username1,Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+            }*/
+    //});
+
 }
