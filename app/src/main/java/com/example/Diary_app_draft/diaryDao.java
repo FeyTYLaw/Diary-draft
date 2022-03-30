9c308b8a9d57c69fa0fe686e8abd562622e9c0dd
Fey <97257705+lawfey@users.noreply.github.com>
2022-03-30 17:54:44 -0400

    Fey TY Law

--- app/src/main/java/com/example/Diary_app_draft/diaryDao.java
+++ app/src/main/java/com/example/Diary_app_draft/diaryDao.java
@@ -1,14 +1,24 @@
 package com.example.Diary_app_draft;
 
+import android.app.Dialog;
+
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
 
 @Dao
 public interface diaryDao {
-    @Query("SELECT* FROM Diary ORER BY id DESC")
-    List<DiaryEntry> getDiaryText();
+    @Query("SELECT* FROM Diary ORDER BY id DESC")
+    List<DiaryEntry> getDiaryInput();
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertDiary(DiaryEntry diaryEntry);
+
+    @Delete
+    void deleteDiaryEntry(DiaryEntry diaryEntry);
 
 }
