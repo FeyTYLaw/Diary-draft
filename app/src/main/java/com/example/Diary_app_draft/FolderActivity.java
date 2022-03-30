1dd112230bf83459525b294cde5e14435c809e39
sumfatema <100000667+sumfatema@users.noreply.github.com>
2022-03-30 18:23:15 -0400

    Fatema Latest 6

--- app/src/main/java/com/example/Diary_app_draft/FolderActivity.java
+++ app/src/main/java/com/example/Diary_app_draft/FolderActivity.java
@@ -2,13 +2,47 @@ package com.example.Diary_app_draft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class FolderActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.folder);
+
+
+        getDiaryText();
     }
+
+    private void getDiaryText(){
+
+        @SuppressLint("StaticFieldLeak")
+        class GetDiaryTask extends AsyncTask<Void, Void, List<DiaryEntry>> {
+            @Override
+            protected List<DiaryEntry> doInBackground(Void... voids) {
+                return DiaryDataBase
+                        .getDatabase(getApplicationContext())
+                        .diaryDao().getAlldiary();
+            }
+
+            @Override
+            protected void onPostExecute(List<DiaryEntry> diaryEntries) {
+                super.onPostExecute(diaryEntries);
+                Log.d("My Diary", diaryEntries.toString());
+
+            }
+        }
+
+        new GetDiaryTask().execute();
+
+    }
+
 }
