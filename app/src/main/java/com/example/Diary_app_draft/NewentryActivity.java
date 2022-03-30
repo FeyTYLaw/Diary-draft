8b63414cf0ec6d1e06b23690f511e492719d4095
Fey <97257705+lawfey@users.noreply.github.com>
2022-03-30 12:00:21 -0400

    Fey TY Law

--- app/src/main/java/com/example/Diary_app_draft/NewentryActivity.java
+++ app/src/main/java/com/example/Diary_app_draft/NewentryActivity.java
@@ -2,18 +2,41 @@ package com.example.Diary_app_draft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class NewentryActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
 
-    private EditText Entry;
-    private TextView Date;
+public class NewentryActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button logout;
+    EditText username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newentry);
+        logout = findViewById(R.id.layout);
+        username = findViewById(R.id.username);
+        logout.setOnClickListener(this);
 
-        Entry = findViewById(R.id.Entry);
+        //Adding current date
+        Calendar calender = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calender.getTime());
 
+        TextView textviewDate = findViewById(R.id.Date);
+        textviewDate.setText(currentDate);
+        String cal = calender.toString();
+
+    }
+    @Override
+    public void onClick(View view){
+        Intent intent = new Intent(this, Signin.class);
+        startActivity(intent);
     }
 }
