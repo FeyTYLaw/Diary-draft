656e652b714e4f6f2cdaa6a48aa44a435981262d
Fey <97257705+lawfey@users.noreply.github.com>
2022-04-01 03:00:12 -0400

    Fey TY Law

--- app/src/main/java/com/example/Diary_app_draft/DiaryAdapter.java
+++ app/src/main/java/com/example/Diary_app_draft/DiaryAdapter.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class DiaryAdapter extends RecyclerView.Adapter<DiaryAdapter.DiaryViewHodler> {
 
     private List<DiaryEntry> Diary;
